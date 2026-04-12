791781fa6bb8fc9631a97681a37fa250757dd0f8
sam <sampurna.chapagainnn@gmail.com>
2026-04-12 18:23:53 +0100

    add user model with role enum and admin and patient method

---
+++ app/models/user.rb
@@ -0,0 +1,23 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  enum :role, {patient:0, admin: 1}
+  after_initialize :set_default_role, if: :new_record?
+
+  private
+
+  def set_default_role
+    self.role ||= :patient
+  end
+
+  def admin?
+    role == "admin"
+  end
+
+  def patient?
+    role == "patient"
+  end
+
+end
