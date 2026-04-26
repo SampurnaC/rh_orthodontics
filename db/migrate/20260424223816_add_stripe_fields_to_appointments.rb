34ba5231c257a5dc2440a3b157ec33665cd2a75e
sam <sampurna.chapagainnn@gmail.com>
2026-04-26 18:30:35 +0100

    add stripe fields to appointments

--- db/migrate/20260424223816_add_stripe_fields_to_appointments.rb
+++ db/migrate/20260424223816_add_stripe_fields_to_appointments.rb
@@ -1,6 +1,8 @@
 class AddStripeFieldsToAppointments < ActiveRecord::Migration[8.0]
   def change
-    add_column :appointments, :stripe_session_id, :string, index: { unique: true }
+    add_column :appointments, :stripe_session_id, :string
+    add_index :appointments, :stripe_session_id, unique: true
+
     add_column :appointments, :payment_status, :string, default: "pending"
   end
 end
